88a452ea095ce8eb661a9abc75cd815bc3bb93b8
GELIOS-19 <64653918+GELIOS-19@users.noreply.github.com>
2022-05-23 10:57:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,33 +7,28 @@ app = Flask(__name__, template_folder="templates")
 # create game service instance
 _game_service = GameService()
 
-""" api urls """
-
 
 # game - initialize a game
 @app.route("/game/initialize-game")
 def initialize_game():
-    _game_service.initialize_game()
-    return jsonify(game=_game_service.dictionary)
+  _game_service.initialize_game()
+  return jsonify(game=_game_service.dictionary)
 
 
 # game - player turn
 @app.route("/game/player-turn/<index>/<player>")
 def player_turn(player: str, index: int):
-    _game_service.player_turn(int(index), Player(player))
-    return jsonify(game=_game_service.dictionary)
+  _game_service.player_turn(int(index), Player(player))
+  return jsonify(game=_game_service.dictionary)
 
 
 # game - get unchanged game object
 @app.route("/game")
 def game():
-    return jsonify(game=_game_service.dictionary)
-
-
-""" view urls """
+  return jsonify(game=_game_service.dictionary)
 
 
 # view - index
 @app.route("/")
 def index():
-    return render_template("index.html", game=_game_service)
+  return render_template("index.html", game=_game_service)
